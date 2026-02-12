3c0425ab28c4db5ae084ab44f94a93aee649ec63
evil <vnikita.2010@gmail.com>
2026-02-12 17:34:08 +0200

    g

---
+++ log.lua
@@ -0,0 +1,13 @@
+local Announcement = [[
+ --- 11 feb 2026
+ [+] added more stuff to skids tab
+ [+] started working to improve exec tab
+ [!] im kinda out of motivation to continue :/
+ [!] also my 'friend' tried stealing the src, now i learnt my lesson to obf
+
+ --- 23 jan 2026
+ [+] Used ob lib
+ [+] added contents in tab with scripts
+ [+] started working on local player
+ [+] fixed key
+]]
