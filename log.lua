6c1825b9f5cf934a4db4fce751fc7581287bc0fc
evil <vnikita.2010@gmail.com>
2026-02-13 20:02:32 +0200

    auuuuu

--- log.lua
+++ log.lua
@@ -1,13 +1,1 @@
-local Announcement = [[
- --- 11 feb 2026
- [+] added more stuff to skids tab
- [+] started working to improve exec tab
- [!] im kinda out of motivation to continue :/
- [!] also my 'friend' tried stealing the src, now i learnt my lesson to obf
-
- --- 23 jan 2026
- [+] Used ob lib
- [+] added contents in tab with scripts
- [+] started working on local player
- [+] fixed key
-]]
+local Announcement = " --- 11 feb 2026\n [+] added more stuff to skids tab\n [+] started working to improve exec tab\n [!] im kinda out of motivation to continue :/\n [!] also my 'friend' tried stealing the src, now i learnt my lesson to obf\n --- 23 jan 2026\n [+] Used ob lib\n [+] added contents in tab with scripts\n [+] started working on local player\n [+] fixed key"
